2014886081b5ce98f451b6b6448dc42d7b248370
Pytwer.Dev <122356458+Pytwer@users.noreply.github.com>
2025-02-06 01:17:06 -0300

    oi

--- questão 15/app15.js
+++ questão 15/app15.js
@@ -1,5 +1,7 @@
-let valor1 = 10;
-let valor2 = 20;
-let resultado = (calculo = valor1 + valor2);
-
+function calculo(valor1,valor2){
+return valor1,valor2;
+}
+valor1 = 10;
+valor2 = 20;
+let resultado = valor1 + valor2;
 console.log(resultado);
